ec0defe93bba99c0946d349201835362d39c7d09
ALINA SICIU <alina.siciu@stud.ubbcluj.ro>
2024-11-19 18:28:18 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SiciuAlinaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
